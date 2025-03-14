2eb65630f509f42f3d121bd48a7f239aaeff9ff5
Hildigerr Vergaray <Hildigerr.Vergaray@YmirSystems.com>
2025-03-14 14:43:24 -0700

    xpdf 3.01pl2

--- xpdf/Stream.h
+++ xpdf/Stream.h
@@ -232,6 +232,8 @@ public:
 
   ~StreamPredictor();
 
+  GBool isOk() { return ok; }
+
   int lookChar();
   int getChar();
 
@@ -249,6 +251,7 @@ private:
   int rowBytes;			// bytes per line
   Guchar *predLine;		// line buffer
   int predIdx;			// current index in predLine
+  GBool ok;
 };
 
 //------------------------------------------------------------------------
@@ -527,7 +530,7 @@ private:
   short getWhiteCode();
   short getBlackCode();
   short lookBits(int n);
-  void eatBits(int n) { inputBits -= n; }
+  void eatBits(int n) { if ((inputBits -= n) < 0) inputBits = 0; }
 };
 
 //------------------------------------------------------------------------
